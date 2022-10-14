cd662fed92dbabfcdcac1b7e1ca9602a20c42613
Marshall Kroske <kroskema@msu.edu>
2022-10-14 15:53:40 -0400

    added chess class

---
+++ app/src/main/java/edu/msu/murraniy/project1/Chess.java
@@ -0,0 +1,10 @@
+package edu.msu.murraniy.project1;
+
+public class Chess {
+    private boolean mTurn = false;
+//    private ChessPiece[] mPieces;
+
+    private boolean checkWin(){
+        return false;
+    }
+}
