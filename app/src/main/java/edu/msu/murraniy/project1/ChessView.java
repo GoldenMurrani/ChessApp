0a7cd372cdf0b5f0af64f7b7a106a9fa30ffb112
natefferry <natefferry@gmail.com>
2022-10-16 18:05:12 -0400

    Initial ChessView class, will require other implementations to be completed for further dev

---
+++ app/src/main/java/edu/msu/murraniy/project1/ChessView.java
@@ -0,0 +1,45 @@
+package edu.msu.murraniy.project1;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Custom view class for Chess.
+ */
+public class ChessView extends View {
+
+    /**
+     * The Chess game
+     */
+    private Chess chess;
+
+    public ChessView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public ChessView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public ChessView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        //chess initialization to be implemented
+        //chess = new Chess;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        //chess drawing to be implemented
+        //chess.draw(canvas);
+    }
+}
