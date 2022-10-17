48be92ee30f7f17f6dfaffdb852d8b841b5bd746
kocabtho <57648313+kocabtho@users.noreply.github.com>
2022-10-17 15:48:17 -0400

    Added ChessPiece class and 2-dimensional array for chesspieces in chess class

--- app/src/main/java/edu/msu/murraniy/project1/Chess.java
+++ app/src/main/java/edu/msu/murraniy/project1/Chess.java
@@ -2,7 +2,9 @@ package edu.msu.murraniy.project1;
 
 public class Chess {
     private boolean mTurn = false;
-//    private ChessPiece[] mPieces;
+    private ChessPiece[] whitePieces;
+    private ChessPiece[] blackPieces;
+    private ChessPiece[] mPieces = [whitePieces, blackPieces];
 
     private boolean checkWin(){
         return false;
---
+++ app/src/main/java/edu/msu/murraniy/project1/ChessPiece.java
@@ -0,0 +1,6 @@
+package edu.msu.murraniy.project1;
+
+public class ChessPiece{
+	private int loc_x;
+	private int loc_y;
+}
