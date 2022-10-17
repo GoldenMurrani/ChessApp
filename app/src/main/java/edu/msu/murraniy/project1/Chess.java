4a471d0b55ba42d45d2ce7a4442d288b8335c412
Naveen Kumanan <kumanann@msu.edu>
2022-10-17 17:09:31 -0400

    Added ChessActivity class and routed start match button to start ChessActivity

--- app/src/main/java/edu/msu/murraniy/project1/Chess.java
+++ app/src/main/java/edu/msu/murraniy/project1/Chess.java
@@ -4,7 +4,7 @@ public class Chess {
     private boolean mTurn = false;
     private ChessPiece[] whitePieces;
     private ChessPiece[] blackPieces;
-    private ChessPiece[] mPieces = [whitePieces, blackPieces];
+//    private ChessPiece[] mPieces = [whitePieces, blackPieces];
 
     private boolean checkWin(){
         return false;
