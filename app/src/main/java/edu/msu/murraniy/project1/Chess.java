735a91f61a50ac41bf11f16d339039a853f80daa
natefferry <natefferry@gmail.com>
2022-10-17 22:40:16 -0400

    Completed chess pieces initialization and drawing

--- app/src/main/java/edu/msu/murraniy/project1/Chess.java
+++ app/src/main/java/edu/msu/murraniy/project1/Chess.java
@@ -5,7 +5,12 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
+import java.util.ArrayList;
+
 public class Chess {
+    // PUBLIC enumeration to represent teams
+    public enum Team { WHITE, BLACK }
+
     // The parent ChessView of this game Chess
     private ChessView parentView;
     // The greenish paint of the board
@@ -14,12 +19,18 @@ public class Chess {
     private Paint grayPaint;
     // The size of the chess board in pixels
     private int chessSize;
-    // Percentage of the display width or height that is occupied by the puzzle
+    // The scaling of the chess pieces
+    private float scaleFactor;
+    // Percentage of the display width or height that is occupied by the chess board
     final static float SCALE_IN_VIEW = 0.9f;
     // The size of the horizontal margin in pixels
     private int marginX;
     // The size of the vertical margin in pixels
     private int marginY;
+    // Represents which team has the turn
+    private Team turn;
+    // Collection of chess pieces
+    private ArrayList<ChessPiece> pieces = new ArrayList<>();
 
     public Chess(Context context, ChessView view) {
         parentView = view;
@@ -29,6 +40,45 @@ public class Chess {
         greenPaint.setColor(0xff18453b);
         grayPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         grayPaint.setColor(0xffb9c7c4);
+
+        // Give the turn to the white team first
+        turn = Team.WHITE;
+
+        // Initialize the chess pieces
+        pieces.add(new ChessPiece(context, R.drawable.rook_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.knight_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.bishop_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.queen_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.king_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.bishop_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.knight_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.rook_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_black, Team.BLACK));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.pawn_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.rook_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.knight_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.bishop_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.queen_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.king_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.bishop_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.knight_white, Team.WHITE));
+        pieces.add(new ChessPiece(context, R.drawable.rook_white, Team.WHITE));
+
+        startingPlacement();
     }
 
     public void draw(Canvas canvas) {
@@ -52,15 +102,24 @@ public class Chess {
             }
         }
 
+        // Determine scaling for the chess pieces
+        scaleFactor = (chessSize/8.0f) / pieces.get(0).getBitmap().getWidth();
+
+        // Draw each of the chess pieces
+        for (ChessPiece piece : pieces) {
+            piece.draw(canvas, marginX, marginY, chessSize, scaleFactor);
+        }
 
     }
 
-    private boolean mTurn = false;
-    private ChessPiece[] whitePieces;
-    private ChessPiece[] blackPieces;
-//    private ChessPiece[] mPieces = [whitePieces, blackPieces];
-
-    private boolean checkWin(){
-        return false;
+    // Place the chess pieces in their starting positions (assuming properly ordered array)
+    public void startingPlacement() {
+        for (int i = 0; i < 32; i++) {
+            ChessPiece piece = pieces.get(i);
+            int offset_team = i < 16 ? 0 : 12;
+            int offset_row = (i%16) < 8 ? 0 : 2;
+            piece.setX(((i%8)*2+1)/16.0f);
+            piece.setY((offset_team+offset_row+1)/16.0f);
+        }
     }
 }
--- app/src/main/java/edu/msu/murraniy/project1/ChessPiece.java
+++ app/src/main/java/edu/msu/murraniy/project1/ChessPiece.java
@@ -1,6 +1,54 @@
 package edu.msu.murraniy.project1;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+
 public class ChessPiece{
-	private int loc_x;
-	private int loc_y;
+
+	// The image for the chess piece
+	private final Bitmap piece;
+	// X location of the chess piece in pixels
+	private float x;
+	// Y location of the chess piece in pixels
+	private float y;
+	// Team of this chess piece
+	private Chess.Team team;
+	// The id of this chess piece
+	private final int id;
+
+
+	public ChessPiece(Context context, int id, Chess.Team team) {
+		this.id = id;
+		this.team = team;
+
+		piece = BitmapFactory.decodeResource(context.getResources(), id);
+	}
+
+	public void draw(Canvas canvas, int marginX, int marginY,
+					 int chessSize, float scaleFactor) {
+		canvas.save();
+
+		// Convert x,y to pixels and add the margin, then draw
+		canvas.translate(marginX + x * chessSize, marginY + y * chessSize);
+
+		// Scale it to the right size
+		canvas.scale(scaleFactor, scaleFactor);
+
+		// This magic code makes the center of the piece at 0, 0
+		canvas.translate(-piece.getWidth() / 2f, -piece.getHeight() / 2f);
+
+		// Draw the bitmap
+		canvas.drawBitmap(piece, 0, 0, null);
+		canvas.restore();
+	}
+
+	public Bitmap getBitmap() { return piece; }
+	public Chess.Team getTeam() { return team; }
+	public float getX() { return x; }
+	public void setX(float x) { this.x = x; }
+	public float getY() { return y; }
+	public void setY(float y) { this.y = y; }
+
 }
