b13e0a5aa506216d9e89d49aecdc5230c35d5ec0
natefferry <natefferry@gmail.com>
2022-11-21 16:25:59 -0500

    Base Cloud stuff

---
+++ app/src/main/java/edu/msu/murraniy/project1/Cloud/ChessService.java
@@ -0,0 +1,12 @@
+package edu.msu.murraniy.project1.Cloud;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface ChessService {
+    
+}
---
+++ app/src/main/java/edu/msu/murraniy/project1/Cloud/Cloud.java
@@ -0,0 +1,35 @@
+package edu.msu.murraniy.project1.Cloud;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
+
+@SuppressWarnings("deprecation")
+public class Cloud {
+    private static final String MAGIC = "NechAtHa6RuzeR8x";
+    private static final String BASE_URL = "https://webdev.cse.msu.edu/~kroskema/cse476/project2/";
+    public static final String CREATEUSER_PATH = "chess-createuser.php";
+    public static final String CREATEGAME_PATH = "chess-creategame.php";
+    public static final String DELETEGAME_PATH = "chess-deletegame.php";
+    public static final String GETGAMESTATE_PATH = "chess-getgamestate.php";
+    private static final String UTF8 = "UTF-8";
+
+    private static Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(SimpleXmlConverterFactory.create())
+            .build();
+
+
+}
+
